7704c6ecea02b1f0c3f49dbb14781f5e62a8de5d
ShenQi1996 <qisam1989@gmail.com>
2021-07-19 14:42:38 -0400

    update projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,6 @@ import "./Projects.css";
 import P1 from "../images/NETFLIX.PNG";
 import P2 from "../images/WEATER.PNG";
 import P3 from "../images/WEB1.PNG";
-import P4 from "../images/AMAZON.PNG";
 import P5 from "../images/java.PNG";
 import P6 from "../images/fullstack.PNG";
 import P7 from "../images/GrillLit.PNG";
@@ -16,43 +15,30 @@ function Projects() {
         <div class="carousel owl-carousel">
           <div class="card">
             <div class="box">
-              <img src={P1} alt="" />
-              <div class="text"> NETFLIX</div>
-              <p>
-                This project I am using ReactJS, HTML, CSS and Firebase to make
-                this project.
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="box">
-              <img src={P2} alt="" />
-              <div class="text">WEATER APP</div>
-              <p>
-                One of my first ReactJS project I have learn so much about
-                ReactJS and HTML.
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="box">
-              <img src={P3} alt="" />
-              <div class="text">WEBPAGE</div>
-              <p>Website That use HTML, CSS, and ReactJS to create.</p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="box">
-              <img src={P4} alt="" />
-              <div class="text">AMAZON-CLONE</div>
-              <p>Amazon-clone only use HTML, CSS, React/Redux and Firebase .</p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="box">
               <img src={P5} alt="" />
               <div class="text">HALF-NOTE</div>
-              <p>Muise maker that only using HTML, CSS, and JavaScript.</p>
+              <p>
+                Half-note is website that creates muise notes The website only
+                using HTML, CSS, and JavaScript.
+              </p>
+              <div className="projects_button">
+                <a
+                  href="https://github.com/ShenQi1996/Half_Note"
+                  target="_blank"
+                  rel=""
+                  className="projects_buttons"
+                >
+                  GitHub
+                </a>
+                <a
+                  href="https://shenqi1996.github.io/Half_Note/"
+                  target="_blank"
+                  rel=""
+                  className="projects_buttons"
+                >
+                  Live
+                </a>
+              </div>
             </div>
           </div>
           <div class="card">
@@ -63,6 +49,24 @@ function Projects() {
                 A website that uses HTML, CSS,ReactJS / Redux as the frontend
                 and Ruby on Rails as the backend.
               </p>
+              <div className="projects_button">
+                <a
+                  href="https://github.com/ShenQi1996/PixelPerfect"
+                  target="_blank"
+                  rel=""
+                  className="projects_buttons"
+                >
+                  GitHub
+                </a>
+                <a
+                  href="https://pixelperfectsam.herokuapp.com/#/"
+                  target="_blank"
+                  rel=""
+                  className="projects_buttons"
+                >
+                  Live
+                </a>
+              </div>
             </div>
           </div>
           <div class="card">
@@ -73,6 +77,81 @@ function Projects() {
                 A Group project that uses HTML,CSS, ReactJS/ Redux as the
                 frontend and MongoDB, Express.js as the the backend{" "}
               </p>
+              <div className="projects_button">
+                <a
+                  href="https://github.com/ShenQi1996/GrillLit"
+                  target="_blank"
+                  rel=""
+                  className="projects_buttons"
+                >
+                  GitHub
+                </a>
+                <a
+                  href="https://grilllit.herokuapp.com/#/"
+                  target="_blank"
+                  rel=""
+                  className="projects_buttons"
+                >
+                  Live
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="box">
+              <img src={P1} alt="" />
+              <div class="text"> NETFLIX</div>
+              <p>
+                This project I am using ReactJS, HTML, CSS and Firebase to make
+                this project.
+              </p>
+              <div className="projects_button">
+                <a
+                  href="https://github.com/ShenQi1996/React-JS-NETFLIX"
+                  target="_blank"
+                  rel=""
+                  className="projects_buttons"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="box">
+              <img src={P2} alt="" />
+              <div class="text">WEATER APP</div>
+              <p>
+                One of my first ReactJS project I have learn so much about
+                ReactJS and HTML.
+              </p>
+              <div className="projects_button">
+                <a
+                  href="https://github.com/ShenQi1996/ReactJS-PJ-Weater-App"
+                  target="_blank"
+                  rel=""
+                  className="projects_buttons"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="box">
+              <img src={P3} alt="" />
+              <div class="text">WEBPAGE</div>
+              <p>Website That use HTML, CSS, and ReactJS to create.</p>
+              <div className="projects_button">
+                <a
+                  href="https://github.com/ShenQi1996/React-JS-Website"
+                  target="_blank"
+                  rel=""
+                  className="projects_buttons"
+                >
+                  GitHub
+                </a>
+              </div>
             </div>
           </div>
         </div>
